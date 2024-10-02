dc31e4d76b923f68b37b4423121db90e5e35e4ca
Maria Mayorova <mary_ss@MacBook-Air-Kaparya.local>
2024-10-02 12:17:00 +0300

    Initial commit

---
+++ TinkoffCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffCalculator
+//
+//  Created by Maria Mayorova on 02.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
